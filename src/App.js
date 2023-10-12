37b5cefcca2449613ec91eea5a6c427da5a36375
Parag Borkar <paragborkar200gmail.com>
2023-10-12 13:58:42 +0530

    Routing Created

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Caregivers from './Components/Caregivers';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 function App() {
   return (
     <div className="App">
      
     <Navbar></Navbar>
-   <Caregivers></Caregivers>
-   <Caregivers></Caregivers>
-   <Caregivers></Caregivers>
-   <Caregivers></Caregivers>
-   <Caregivers></Caregivers>
-
+   <Router>
+    <Routes>
+    <Route path="/" exact component={<Login/>} />
+    <Route path="/signup" exact component={<Signup/>} />
+     </Routes>
+   </Router>
 
     </div>
   );
