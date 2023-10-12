eb2b00b9916db2122c679a071466253aac616bb5
Parag Borkar <paragborkar200gmail.com>
2023-10-12 14:11:16 +0530

    Routing Changed

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Caregivers from './Components/Caregivers';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+function App() {
+  return (
+    <div className="App">
+     
+    <Navbar/>
+   <Router>
+    <Routes>
+    <Route path="/"  element={<Login/>} />
+    <Route path="/signup" element={<Signup/>} />
+    <Route path="/home" element={<Caregivers/>} />
+     </Routes>
+   </Router>
+    </div>
+  );
+}
+
+export default App;
