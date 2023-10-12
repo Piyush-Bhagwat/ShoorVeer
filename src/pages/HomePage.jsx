49afa12fb2c984789b1e88a14a918ea1c7c72bca
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2023-10-12 17:58:51 +0530

    edited HomePage Title section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,15 @@ import React from "react";
 function HomePage() {
     return (
         <div className="home-page">
-            <h3>Welcome,</h3>
-            <h1 className="user-name">Piyush Bhagwat</h1>
+            <div className="title-box">
+                <h3>Welcome,</h3>
+                <h1 className="user-name">Piyush Bhagwat</h1>
+            </div>
 
+            <div className="location">
+                <i className="fa-solid fa-location-dot"></i>
+                Bajaj Nagar, Nagpur
+            </div>
         </div>
     );
 }
